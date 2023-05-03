a92378e965429c58a1160af113452726dd58f913
lalitha2407 <lalitha@bootlabstech.com>
2023-05-04 01:09:08 +0530

    initial

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
-# Creates a Flexible MySQL Server
 resource "azurerm_mysql_flexible_server" "example" {
-  for_each= { for i in var.server_set : i.name => i }
-  name                         = each.value.name
+  name                         = var.server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
-  administrator_login          = each.value.administrator_login
-  administrator_password       = each.value.administrator_login_password
-  backup_retention_days        = each.value.backup_retention_days
-  create_mode                  = each.value.create_mode
+  administrator_login          = var.administrator_login
+  administrator_password       = var.administrator_login_password
+  backup_retention_days        = var.backup_retention_days
+  create_mode                  = var.create_mode
   geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
-  sku_name                     = each.value.sku_name
-  version                      = each.value.mysql_version
+  sku_name                     = var.sku_name
+  version                      = var.mysql_version
   # identity {
   #   type = "SystemAssigned"
   # }
-  storage {
-    auto_grow_enabled = each.value.auto_grow_enabled
-    iops = each.value.iops
-    size_gb = each.value.size_gb
-  }
+  # storage {
+  #   auto_grow_enabled = 
+  #   iops = 
+  #   size_gb = 
+  # }
   # high_availability {
   #   mode = 
   #   standby_availability_zone =     
@@ -29,4 +27,20 @@ resource "azurerm_mysql_flexible_server" "example" {
   #   start_minute = 
   # }
 
+}
+# Creates a Flexible MySQL database
+resource "azurerm_mysql_database" "example" {
+  name                = var.name
+  resource_group_name = azurerm_mysql_flexible_server.example.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.example.name
+  charset             = var.charset
+  collation           = var.collation
+}
+# Network setting allows All Azure Services
+resource "azurerm_mysql_flexible_server_firewall_rule" "example" {
+  name                = "office"
+  resource_group_name = azurerm_mysql_flexible_server.example.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-# # Server
-# variable "server_name" {
-#   type        = string
-#   description = "value"
+# Server
+variable "server_name" {
+  type        = string
+  description = "value"
 
-# }
+}
 variable "location" {
   type        = string
   description = "value"
@@ -14,53 +14,69 @@ variable "resource_group_name" {
   description = "value"
 
 }
-# variable "sku_name" {
-#   type        = string
-#   description = "value"
+variable "sku_name" {
+  type        = string
+  description = "value"
 
-# }
-# variable "mysql_version" {
-#   type        = string
-#   description = "value"
-#   default     = "8.0.21"
+}
+variable "mysql_version" {
+  type        = string
+  description = "value"
+  default     = "8.0.21"
 
-# }
-# variable "administrator_login" {
-#   type        = string
-#   description = "value"
+}
+variable "administrator_login" {
+  type        = string
+  description = "value"
 
-# }
-# variable "administrator_login_password" {
-#   type        = string
-#   description = "value"
+}
+variable "administrator_login_password" {
+  type        = string
+  description = "value"
 
-# }
+}
 variable "backup_retention_days" {
   type        = number
   description = "value"
   default     = 35
 
 }
-# variable "create_mode" {
-#   type        = string
-#   description = "value"
-#   default     = "Default"
-
-# }
-variable "server_set" {
-  type        = list(any)
+variable "create_mode" {
+  type        = string
   description = "value"
-
+  default     = "Default"
 
 }
-# variable "storage_mb" {
-#   type        = number
+# variable "public_network_access_enabled" {
+#   type        = bool
 #   description = "value"
+#   default     = true
 
 # }
+variable "storage_mb" {
+  type        = number
+  description = "value"
+
+}
 variable "geo_redundant_backup_enabled" {
   type        = string
   description = "value"
   default     = "false"
+
+}
+# Database
+variable "name" {
+  type        = string
+  description = "value"
+
+}
+variable "charset" {
+  type        = string
+  description = "value"
+
+}
+variable "collation" {
+  type        = string
+  description = "value"
 
 }
