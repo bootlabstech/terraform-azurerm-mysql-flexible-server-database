c944c168d4ac7953e89fe757cef77233a62499d9
lalitha2407 <lalitha@bootlabstech.com>
2023-05-10 09:07:20 +0530

    list of obj

--- variables.tf
+++ variables.tf
@@ -1,9 +1,89 @@
-# Server
-variable "server_name" {
-  type        = string
-  description = "value"
+# # Server
+# variable "server_name" {
+#   type        = string
+#   description = "value"
 
-}
+# }
+# variable "location" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "resource_group_name" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "sku_name" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "mysql_version" {
+#   type        = string
+#   description = "value"
+#   default     = "8.0.21"
+
+# }
+# variable "administrator_login" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "administrator_login_password" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "backup_retention_days" {
+#   type        = number
+#   description = "value"
+#   default     = 35
+
+# }
+# variable "create_mode" {
+#   type        = string
+#   description = "value"
+#   default     = "Default"
+
+# }
+# # variable "public_network_access_enabled" {
+# #   type        = bool
+# #   description = "value"
+# #   default     = true
+
+# # }
+# variable "storage_mb" {
+#   type        = number
+#   description = "value"
+
+# }
+# variable "geo_redundant_backup_enabled" {
+#   type        = string
+#   description = "value"
+#   default     = "false"
+
+# }
+# # Database
+# variable "name" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "charset" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "collation" {
+#   type        = string
+#   description = "value"
+
+# }
+
+
+# list of server
+
 variable "location" {
   type        = string
   description = "value"
@@ -14,69 +94,23 @@ variable "resource_group_name" {
   description = "value"
 
 }
-variable "sku_name" {
-  type        = string
-  description = "value"
-
-}
-variable "mysql_version" {
-  type        = string
-  description = "value"
-  default     = "8.0.21"
-
-}
-variable "administrator_login" {
-  type        = string
-  description = "value"
-
-}
-variable "administrator_login_password" {
-  type        = string
-  description = "value"
-
-}
 variable "backup_retention_days" {
   type        = number
   description = "value"
   default     = 35
 
 }
-variable "create_mode" {
-  type        = string
-  description = "value"
-  default     = "Default"
 
-}
-# variable "public_network_access_enabled" {
-#   type        = bool
-#   description = "value"
-#   default     = true
-
-# }
-variable "storage_mb" {
-  type        = number
+variable "server_set" {
+  type        = list(any)
   description = "value"
 
+
 }
+
 variable "geo_redundant_backup_enabled" {
   type        = string
   description = "value"
   default     = "false"
-
-}
-# Database
-variable "name" {
-  type        = string
-  description = "value"
-
-}
-variable "charset" {
-  type        = string
-  description = "value"
-
-}
-variable "collation" {
-  type        = string
-  description = "value"
 
 }
