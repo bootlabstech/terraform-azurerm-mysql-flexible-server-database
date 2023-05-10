0f8a794bc148eacf0a48015ad60f8dca5cd58328
lalitha2407 <lalitha@bootlabstech.com>
2023-05-10 16:29:05 +0530

    keyvault data block

--- main.tf
+++ main.tf
@@ -1,69 +1,21 @@
-# single server
-# resource "azurerm_mysql_flexible_server" "example" {
-#   name                         = var.server_name
-#   resource_group_name          = var.resource_group_name
-#   location                     = var.location
-#   administrator_login          = var.administrator_login
-#   administrator_password       = var.administrator_login_password
-#   backup_retention_days        = var.backup_retention_days
-#   create_mode                  = var.create_mode
-#   geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
-#   sku_name                     = var.sku_name
-#   version                      = var.mysql_version
-#   # identity {
-#   #   type = "SystemAssigned"
-#   # }
-#   # storage {
-#   #   auto_grow_enabled = 
-#   #   iops = 
-#   #   size_gb = 
-#   # }
-#   # high_availability {
-#   #   mode = 
-#   #   standby_availability_zone =     
-#   # }
-#   # maintenance_window {
-#   #   day_of_week = 
-#   #   start_hour = 
-#   #   start_minute = 
-#   # }
-
-# }
-# # Creates a Flexible MySQL database
-# resource "azurerm_mysql_database" "example" {
-#   name                = var.name
-#   resource_group_name = azurerm_mysql_flexible_server.example.resource_group_name
-#   server_name         = azurerm_mysql_flexible_server.example.name
-#   charset             = var.charset
-#   collation           = var.collation
-# }
-
-
-resource "random_password" "password" {
- length = 16
- special = true
-}
-# Creates a list of Flexible MySQL Server
 resource "azurerm_mysql_flexible_server" "example" {
-  for_each= { for i in var.server_set : i.name => i }
-  name                         = each.value.name
+  name                         = var.server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
-  administrator_login          = each.value.administrator_login
+  administrator_login          = var.administrator_login
   administrator_password       = random_password.password.result
-  backup_retention_days        = each.value.backup_retention_days
-  create_mode                  = each.value.create_mode
-  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
-  sku_name                     = each.value.sku_name
-  version                      = each.value.mysql_version
+  backup_retention_days        = var.backup_retention_days
+  create_mode                  = var.create_mode
+  sku_name                     = var.sku_name
+  version                      = var.mysql_version
   # identity {
   #   type = "SystemAssigned"
   # }
-  storage {
-    auto_grow_enabled = each.value.auto_grow_enabled
-    iops = each.value.iops
-    size_gb = each.value.size_gb
-  }
+  # storage {
+  #   auto_grow_enabled = 
+  #   iops = 
+  #   size_gb = 
+  # }
   # high_availability {
   #   mode = 
   #   standby_availability_zone =     
@@ -75,11 +27,34 @@ resource "azurerm_mysql_flexible_server" "example" {
   # }
 
 }
-# Network setting allows All Azure Services
-resource "azurerm_mysql_flexible_server_firewall_rule" "example" {
-  name                = "office"
-  resource_group_name = azurerm_mysql_flexible_server.example.resource_group_name
-  server_name         = azurerm_mysql_flexible_server.example[each.key]
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+# Creates a Flexible MySQL database
+# resource "azurerm_mysql_database" "example" {
+#   name                = var.name
+#   resource_group_name = azurerm_mysql_flexible_server.example.resource_group_name
+#   server_name         = azurerm_mysql_flexible_server.example.name
+#   charset             = var.charset
+#   collation           = var.collation
+# }
+resource "random_password" "password" {
+ length = 16
+ special = true
+}
+resource "azurerm_key_vault_secret" "mysql_username" {
+ name = "${var.name}-username"
+ value = azurerm_mysql_flexible_server.example.administrator_login
+ key_vault_id = data.azurerm_key_vault.key_vault.id
+
+}
+
+
+resource "azurerm_key_vault_secret" "mysql_password" {
+ name  = "${var.name}-password"
+ value = random_password.password.result
+ key_vault_id = data.azurerm_key_vault.key_vault.id
+
+}
+
+data "azurerm_key_vault" "key_vault" {
+ name  = var.keyvault_name
+ resource_group_name = var.resource_group_name
 }
