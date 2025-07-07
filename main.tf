0ab04fb801cb71e048423654f6ffb8c4bb72327f
lalithaa2407 <lalitalallu02@gmail.com>
2025-07-07 12:18:56 +0530

    updated module provider version >4.0.0

--- main.tf
+++ main.tf
@@ -18,16 +18,32 @@ resource "azurerm_mysql_flexible_server" "example" {
   }
 
 }
-
-# Allows all azure services to connect to the server
-resource "azurerm_mysql_flexible_server_firewall_rule" "firewall_rule" {
-  name                = "${var.name}-firewallrule"
+# Creates  a private endpoint with private dns
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  server_name         = var.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  subnet_id           = var.private_endpoint_subnet_id
 
-  depends_on = [azurerm_mysql_flexible_server.example]
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = azurerm_mysql_flexible_server.example.id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+  depends_on = [ azurerm_mysql_flexible_server.example ]
+
 }
 
 # get key vault details to store DB password as secret
--- variables.tf
+++ variables.tf
@@ -53,3 +53,34 @@ variable "zone" {
   description = "Specifies the Availability Zone in which this MySQL Flexible Server should be located. Possible values are 1, 2 and 3."
   default     = "1"
 }
+
+# PE
+# PE
+# private endpoint
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+  default = [ "mysqlServer" ]
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+
+
+
+
